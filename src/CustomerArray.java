0fde4259bfbe772dca163a66ee4c99d04d05835e
User <ntgraall@gmail.com>
2019-10-23 21:41:05 +0300

    Home work7

--- src/CustomerArray.java
+++ src/CustomerArray.java
@@ -3,11 +3,11 @@ import java.util.Objects;
 
 public class CustomerArray {
     private static int count;
-    private Collection[]  customerArray;
+    private String[]  customerArray;
 
     public CustomerArray() {
 
-        customerArray = new Collection[10];
+        customerArray = new String[10];
     }
 
     @Override
@@ -19,23 +19,33 @@ public class CustomerArray {
         return this.customerArray[index].toString();
     }
 
+    /* Если индекс будет больше размерности массива данных
+     в методе add после того как запись вставлена проверяется
+     длина массива со значением счетчика, который возрастает на 1 после каждого добавления.
+     Если счетчик достиг длины массива создается новый массив копированием из предыдущего с увеличенной длиной.
+    */
     public void add(String value){
 
-        this.customerArray[count] = new Collection(value, count);
+        this.customerArray[count] = value;
          count++;
 
           if ( this.customerArray.length == count){
-              Collection[]  newCustomerArray = new Collection[this.customerArray.length + (this.customerArray.length * 60 / 100)];
+              String[]  newCustomerArray = new String[this.customerArray.length + (this.customerArray.length * 60 / 100)];
               System.arraycopy(this.customerArray, 0, newCustomerArray, 0, this.customerArray.length);
               customerArray = newCustomerArray;
           }
     }
 
-    public int indexOf(Collection item){
-        return item.getId();
+    public int indexOf(String item){
+        for(int i = 0; i < this.customerArray.length; i++){
+            if((this.customerArray[i]) == item) {
+                return i;
+            }
+        }
+        return 0;
     }
 
-    public Collection get(int index){
+    public String get(int index){
 
         for(int i = 0; i < this.customerArray.length; i++){
             if(indexOf(this.customerArray[i]) == index) {
@@ -45,16 +55,16 @@ public class CustomerArray {
         return null;
     }
 
-    public void delete(Collection item) {
+    public void delete(String item) {
         int index = indexOf(item);
-        Collection[] newCustomerArray = new Collection[this.customerArray.length];
+        String[] newCustomerArray = new String[this.customerArray.length];
         System.arraycopy(this.customerArray, 0, newCustomerArray, 0, index);
         System.arraycopy(this.customerArray, index + 1, newCustomerArray, index, this.customerArray.length - index - 1);
         this.customerArray = newCustomerArray;
     }
 
     public boolean contains(String value){
-        for (Collection element : this.customerArray){
+        for (String element : this.customerArray){
 
         if (element == null){
             continue;
@@ -67,6 +77,7 @@ public class CustomerArray {
         return false;
     }
 
+    //Метод очистить
     public void  clear(){
         Arrays.fill(this.customerArray, null);
     }
@@ -75,12 +86,12 @@ public class CustomerArray {
         return this.customerArray.length;
     }
 
-    public boolean equals(Object o1, Object o2){
+    public boolean equals(String o1, String o2){
         if (o1 == o2) return true;
         if (o1 == null || o2 == null || o1.getClass() != o2.getClass()) return false;
-        Collection item1 = (Collection) o1;
-        Collection item2 = (Collection) o2;
-        return Objects.equals(indexOf(item1), indexOf(item2));
+        String item1 = (String) o1;
+        String item2 = (String) o2;
+        return Objects.equals(item1, item2);
     }
 
 }
